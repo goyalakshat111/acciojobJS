fef8740735f3d4640eadad84112ce9df92e8032a
Akshat Goyal <goyalakshat111@gmail.com>
2024-12-08 15:35:17 +0530

    for loop

--- 5code.js
+++ 5code.js
@@ -1,15 +1,13 @@
 // 18 nov
 
-
 // increment and decrement operator
 
 // let a=5
-
 // z= a++ + --a
-
 // console.log(z)
 // console.log(a)
 
+// **********************************************************************
 
 // for loop - all three loops answer would be same
 
@@ -25,21 +23,47 @@
 //     console.log(i)
 // }
 
-
+// **************************************************************************
 
 // prime number
 
-let a = 101
-let ans = true
-  
-  for(let i=2;i<a;i++){
-    if(a%i==0){
-       ans = false
-       break;
-    }
-  }
-  
-   if(a==2){
-    ans = true
-  }
- console.log(ans)
+// approach-1
+
+// let a = 110
+// let ans=true;
+// if(a==2){
+//   ans = true
+// }
+// if(a<2){
+//   ans = false;
+// }
+// for(let i=2;i<a;i++){
+//   if(a%i==0){
+//     ans = false
+//     break;
+//   }
+// }
+// if(ans){
+//   console.log('Prime')
+// }
+// else{
+//   console.log('Non-prime')
+// }
+
+// approach-2
+
+// let num=101
+// let count=0;
+// for(let i=1;i<=num;i++ ){
+//   if(num%i==0){
+//     count++;
+//   }
+// }
+// if(count==2){
+//   console.log('Prime')
+// }
+// else{
+//   console.log('Non-prime')
+// }
+
+// approach -3 To optimize, run loop from 0 to Math.sqrt(n)
