df8b19566a7294a87a1123fb2586b530bc25bd65
Akshat Goyal <goyalakshat111@gmail.com>
2024-12-14 18:56:52 +0530

    complexity

---
+++ 20code.js
@@ -0,0 +1,12 @@
+// 13 dec
+
+// Complexity - 
+// 1. Time Complexity - Worst Case(BigO), Avergae Case(Theta), Best Case(Omega)
+// 2. Space Complexity - Worst Case(BigO), Avergae Case(Theta), Best Case(Omega)
+
+// Time Complexity - Time complexity describes how fast your program runs as the input size 
+//(like the length of your array) increases. 
+
+// Space complexity - Space complexity describes how much extra memory your program needs.
+
+// For more : https://virtualcoder.site/complexity-space-and-time-2/
